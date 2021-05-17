95b52c0529b41aedd54d1e45fc9d55a955678ec5
erikdao <cuong@neuraltalks.io>
2021-05-17 10:18:39 +0200

    WIP: defining encoder-decoder for LSTM

--- lstm/model.py
+++ lstm/model.py
@@ -16,6 +16,25 @@ torch.backends.cudnn.benchmark = False
 MAX_OUTPUT_LENGTH = 256
 
 
+class Encoder(nn.Module):
+    """
+    The Encoder is a bidirectional LSTM
+    """
+    def __init__(self, embedding_dim, encoder_hidden_dim, decoder_hidden_dim):
+        super().__init__()
+
+        self.lstm = nn.LSTM(embedding_dim, encoder_hidden_dim, bidirectional=True)
+        self.fc = nn.Linear(encoder_hidden_dim * 2, decoder_hidden_dim)
+
+    def forward(self, text, text_len):
+        pass
+
+
+class Decoder(nn.Module):
+    def __init__(self):
+        pass
+
+
 class LSTMGenerator(pl.LightningModule):
     def __init__(self, input_size=256, hidden_size=256):
         super().__init__()
